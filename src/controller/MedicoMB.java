983c1d11404df2ace09574b4723343f8d29b57b3
NATALIA <NATALIA@NATALIA-PC>
2016-06-07 23:53:25 -0300

    Medico MB

--- src/controller/MedicoMB.java
+++ src/controller/MedicoMB.java
@@ -9,8 +9,11 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 
+import entity.Especialidade;
 import entity.Medico;
+import persistencia.EspecialidadeDao;
 import persistencia.MedicoDao;
 
 @ManagedBean(name="medmb")
@@ -22,39 +25,85 @@ public class MedicoMB implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private MedicoDao medDao;
-	private Medico med;
+	private Especialidade esp;
+	private MedicoDao medDao = new MedicoDao();
+	private Medico med = new Medico();
+	private boolean editar;
 	private List<Medico> lista = new ArrayList<Medico>();
 	
-	/*public MedicoMB(){
-		med = new Medico();
-		medDao =new MedicoDao();
+	
+	public MedicoMB(){
+		setEditar(false);
+		setMed(new Medico());
 		try {
-			
 			setLista(medDao.listarMedico());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public List<SelectItem> getEspecialidades() {
+		EspecialidadeDao espDao = new EspecialidadeDao();
+		List<Especialidade> especialidades = null;
+		try {
+			especialidades = espDao.consultarEspecialidade();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		List<SelectItem> itens = new ArrayList<SelectItem>(especialidades.size());
+
+		for (Especialidade e : especialidades) {
+			itens.add(new SelectItem(e.getIdEspecialidade(), e.getEspecialidade()));
+		
+		}
+
+		return itens;
+	}
+	
+	public void salvar() {
+		FacesMessage msg = null;
+		try {
+			if (editar) {
 			
+				medDao.alterarMedico(med);
+				msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Medico alterado com sucesso", "");
+				setEditar(false);
+			} else {
+				
+				medDao.adicionarMedico(med);
+				msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Medico adicionado com sucesso", "");
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		med = new Medico();
+		
+		try {
+			setLista(medDao.listarMedico());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String alterar(Medico med) {
+		String pagina = "ConsultaMedicamento";
+		try {
+			setMed(medDao.consultaMedicoAlterar(med.getCrm()));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}*/
-	
-	
+		setEditar(true);
+		pagina = "medico?faces-redirect=true";
 
-	
-	public void adicionar(){
-		try {
-			medDao.adicionarMedico(med);
-			med = new Medico();
-			
-			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Medico adicionado com sucesso", "");
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
-		
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Medico alterado com sucesso", "");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+
+		return pagina;
 	}
 
 	public void remover(){
@@ -99,6 +148,24 @@ public class MedicoMB implements Serializable{
 	}
 	public List<Medico> getLista() {
 		return lista;
+	}
+
+	public Especialidade getEsp() {
+		return esp;
+	}
+
+	public void setEsp(Especialidade esp) {
+		this.esp = esp;
+	}
+
+
+	public boolean isEditar() {
+		return editar;
+	}
+
+
+	public void setEditar(boolean editar) {
+		this.editar = editar;
 	}
 
 
